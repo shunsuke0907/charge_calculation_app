b90775519e3ce3e6ef5c8bfb4db3e2044a997b55
shunsuke0907 <if.0907@i.softbank.jp>
2019-04-03 11:16:58 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す。
+  def full_title(page_title = '')
+    base_title = 'EngineerQUIZ'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
